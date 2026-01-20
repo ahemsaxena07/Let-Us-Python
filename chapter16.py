0e70dbde8875d108ca8b95ba707cc1913dfc6ae7
AHEM SAXENA <99779094+ahemsaxena07@users.noreply.github.com>
2026-01-20 21:03:36 +0530

    last progress
    starting learning modules and packages, added some concepts on them.

---
+++ chapter16.py
@@ -0,0 +1,26 @@
+# #modules and packages
+
+# def display():
+#     print('You cannot make History if you use Incognito Mode')
+# def show():
+#     print('Pizza is a pie chart of how much pizza is left')
+# print(__name__)
+# display()
+# show()
+
+# import math
+# import random
+# import functions 
+# a = 100
+# b = 200
+# print(__name__)
+# print(math.sin(0.5))
+# print(math.cos(0.5))
+# print(random.random())
+# print(random.randint(30, 45))
+# functions.display()
+# functions.show()
+
+import sys
+for p in sys.path:
+    print(p)
---
+++ functions.py
@@ -0,0 +1,14 @@
+# def display():
+#     print('Earlier rich owned cars, while poor had horses')
+# def show():
+#     print('Now everyone has car, while only rich own horses')
+    
+def display():
+    print('Wright Brothers are responsible for 9/11 too')
+def main():
+    print('If you beat your own record, you win as well as lose')
+    print('Internet connects people at a long distance')
+    print('Internet disconnects people at a short distance')
+    display()
+if __name__ == '__main__':
+    main()
